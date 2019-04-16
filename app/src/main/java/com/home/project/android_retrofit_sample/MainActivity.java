a0848f524249778196c34e5b93bc73e13bb678a4
Hbeto021 <humberto.cesar01@outlook.com>
2019-04-15 21:34:44 -0300

    Commit project

---
+++ app/src/main/java/com/home/project/android_retrofit_sample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.home.project.android_retrofit_sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
