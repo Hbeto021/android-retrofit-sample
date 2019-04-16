b4bbc61492e2cd64830f28c1e13451d3f85a1f02
Hbeto021 <humberto.cesar01@outlook.com>
2019-04-16 08:58:55 -0300

    Defining my model class

---
+++ app/src/main/java/com/home/project/android_retrofit_sample/domain/model/City.java
@@ -0,0 +1,32 @@
+package com.home.project.android_retrofit_sample.domain.model;
+
+public class City {
+
+    private String name;
+    private String state;
+    private String country;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
