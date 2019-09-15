63d43291c4631bb6f7b3ff81148e4061263e198f
humberto.c.tolintino <humberto.c.tolintino@accenture.com>
2019-09-15 14:38:20 -0300

    Removing unused methods

--- app/src/main/java/com/home/project/android_retrofit_sample/domain/model/City.java
+++ app/src/main/java/com/home/project/android_retrofit_sample/domain/model/City.java
@@ -7,40 +7,20 @@ public class City {
     private String country;
 
     public String getName() {
-        if(name == null) {
-            name = "";
-        }
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getState() {
-        if(state == null){
-            state = "";
-        }
         return state;
     }
 
-    public void setState(String state) {
-        this.state = state;
-    }
-
     public String getCountry() {
-        if(country == null){
-            country = "";
-        }
         return country;
     }
 
-    public void setCountry(String country) {
-        this.country = country;
-    }
 
     @Override
-    public String toString()    {
+    public String toString() {
 
         return getName() + " / " + getState() + " / " + getCountry();
     }
