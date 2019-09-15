311e0a417b53e9a9b529f55d0a2b7d7d4005e0d5
humberto.c.tolintino <humberto.c.tolintino@accenture.com>
2019-09-15 14:39:36 -0300

    Creating interface for MainActivity

---
+++ app/src/main/java/com/home/project/android_retrofit_sample/view/IMainActivity.java
@@ -0,0 +1,10 @@
+package com.home.project.android_retrofit_sample.view;
+
+public interface IMainActivity {
+
+    void initViews();
+    void onClickViews();
+    void resetFields();
+    void hideComponentsAndShowProgress();
+    void showComponents();
+}
